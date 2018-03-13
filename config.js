50d7bd2a24c0bedda6d742d8205c9ad13f38ed8c
BradHemmerick <Brad.hemmerick13@gmail.com>
2018-03-13 16:09:38 -0500

    tweak to config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     database:
-      'mongodb://admin:password@ds259768.mlab.com:59768/code-tutor',
+    process.env.MONGODB_URI || 'mongodb://admin:password@ds259768.mlab.com:59768/code-tutor',
     port: process.env.port || 3000,
     secret: 'ijiwjid'
   };
